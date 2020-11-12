95592fc1f1167ee9524fea2fc45dc2f478a4e02c
onestep90 <onestep.n90@gmail.com>
2020-11-12 16:38:51 +0900

    Add AspDotnet.MariaDb

---
+++ dotnet-core/Projects/MariaDbCrud/MariaDbCrud/Models/MariaDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+
+namespace MariaDbCrud.Models
+{
+    public partial class MariaDbContext : Microsoft.EntityFrameworkCore.DbContext       
+    {
+        public MariaDbContext(DbContextOptions<MariaDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<WeatherForecastDataModel> WeatherForecasts { get; set; }
+    }
+
+ 
+}
---
+++ dotnet-core/Projects/MariaDbCrud/MariaDbCrud/Services/IMariaDbWeatherForecastService.cs
@@ -0,0 +1,21 @@
+﻿using MariaDbCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MariaDbCrud.Services
+{
+    public interface IMariaDbWeatherForecastService
+    {
+        public Task<int> Delete(int id);
+
+        public Task<IEnumerable<WeatherForecastDataModel>> FindAll();
+
+        public Task<WeatherForecastDataModel> FindOne(int id);
+
+        public Task<int> Insert(WeatherForecastDataModel forecast);
+
+        public Task<int> Update(WeatherForecastDataModel forecast);
+    }
+}
